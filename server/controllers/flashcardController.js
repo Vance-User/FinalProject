96dbe89dd0c2a7eb5eb7404f5aec5eac7746efae
Vance-User <2018118874@ub.edu.bz>
2025-04-26 03:00:58 +0000

    Cards being able to save in databse

--- server/controllers/flashcardController.js
+++ server/controllers/flashcardController.js
@@ -1,12 +1,27 @@
-const Flashcard = require('../models/flashcardModel');
+const db = require('../db');
 
+// GET all flashcards
 exports.getAllFlashcards = async (req, res) => {
-  const cards = await Flashcard.getAll();
-  res.json(cards);
+  try {
+    const result = await db.query('SELECT * FROM flashcards ORDER BY id DESC');
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error fetching flashcards:', err.message);
+    res.status(500).json({ error: err.message });
+  }
 };
 
+// POST a new flashcard
 exports.createFlashcard = async (req, res) => {
   const { question, answer } = req.body;
-  const newCard = await Flashcard.create({ question, answer });
-  res.status(201).json(newCard);
+  try {
+    const result = await db.query(
+      'INSERT INTO flashcards (question, answer) VALUES ($1, $2) RETURNING *',
+      [question, answer]
+    );
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error('Error creating flashcard:', err.message);
+    res.status(500).json({ error: err.message });
+  }
 };
--- server/routes/flashcardRoutes.js
+++ server/routes/flashcardRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const flashcardController = require('../controllers/flashcardController');
+const db = require('../db');
+
 
 router.get('/', flashcardController.getAllFlashcards);
 router.post('/', flashcardController.createFlashcard);
